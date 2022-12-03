28f4069bdb17362d6517dad4956bea5fb6f0ec0a
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-03 16:14:57 -0600

    Refactor solutions library.js

--- src/Library.js
+++ src/Library.js
@@ -7,31 +7,29 @@ function createLibrary(nameOfLib){
 
 function addBook (nameOfLib, bookToAdd){
   if(bookToAdd.genre === 'fantasy'){
-  return nameOfLib.shelves.fantasy.push(bookToAdd);
+    nameOfLib.shelves.fantasy.push(bookToAdd);
   } else if (bookToAdd.genre === 'fiction'){
-    return nameOfLib.shelves.fiction.push(bookToAdd);
+    nameOfLib.shelves.fiction.push(bookToAdd);
   } else if (bookToAdd.genre === 'nonFiction'){
-    return nameOfLib.shelves.nonFiction.push(bookToAdd);
+    nameOfLib.shelves.nonFiction.push(bookToAdd);
   } 
 }
 
+// function addBook(nameOfLib, bookToAdd){
+//   nameOfLib.shelves[bookToAdd.genre].push(bookToAdd);
+// }
+
 function checkoutBook(libObj, bookRequested, genreShelf){
-  for  (var i = 0; i < 5; i++){
-    if (libObj.shelves.fantasy.length === 0){
+  for  (var i = 0; i < genreShelf.length; i++){
+    if (libObj.shelves[genreShelf].length === 0){
       return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`
-    }else if (genreShelf === 'fiction' && libObj.shelves.fiction[i].title === bookRequested){
-      libObj.shelves.fiction.pop();
+    } else if (libObj.shelves[genreShelf][i].title === bookRequested){
+      libObj.shelves[genreShelf].pop();
       return `You have now checked out ${bookRequested} from the ${libObj.name}`
-    } else if (genreShelf === 'nonFiction' && libObj.shelves.nonFiction[i].title === bookRequested) {
-      libObj.shelves.nonFiction.pop();
-      return `You have now checked out ${bookRequested} from the ${libObj.name}`
-    } else if (genreShelf === 'fantasy' && libObj.shelves.fantasy[i].title === bookRequested) {
-      libObj.shelves.fantasy.pop();
-    } else {
-      return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`
     }
   }
 }
+
 
 
 
