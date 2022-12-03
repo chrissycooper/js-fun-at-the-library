9b2b9f205f4ea4d0957dd4f35a63772dc1b39fbf
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-02 21:52:05 -0600

    Pass searchShelf tests, complete shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,33 +1,45 @@
 function shelfBook(book, shelf){
   if (shelf.length < 3){
     return shelf.unshift(book);
-  }  
+  }; 
 };
 
 function unshelfBook(bookTitle, shelf){  
   for (i = 0; i < shelf.length; i++){
     if (bookTitle === shelf[i].title){
       shelf.splice(i, 1);
-    } 
-  }
+    }; 
+  };
   return shelf;
 };
 
 function listTitles(shelf){
-  var listOfTitles = ""
+  var listOfTitles = "";
   for (i = 0; i < shelf.length; i++){
     if (i === 0){
       listOfTitles = shelf[i].title;
     } else {
       listOfTitles = listOfTitles + ", " + shelf[i].title;
-    }
-  }
+    };
+  };
   return listOfTitles;
-}
+};
+
+function searchShelf(shelf, lookingFor){
+  for (i = 0; i < shelf.length; i++){
+    if(shelf[i].title === lookingFor){
+      return true;
+    }; 
+  };
+  return false;
+  };
+  
+    
+ 
 
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
