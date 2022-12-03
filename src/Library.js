245204a0e9649ccb52b134bf862cae6c0c12066b
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-03 16:39:42 -0600

    Refactor checkoutBook function in library-test.js

--- src/Library.js
+++ src/Library.js
@@ -15,9 +15,21 @@ function addBook (nameOfLib, bookToAdd){
   } 
 }
 
+// function checkoutBook(libObj, bookRequested, genreShelf){
+//   for  (var i = 0; i < genreShelf.length; i++){
+//     if (libObj.shelves[genreShelf].length === 0){
+//       return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`
+//     } else if (libObj.shelves[genreShelf][i].title === bookRequested){
+//       libObj.shelves[genreShelf].splice(i, 1);
+//       return `You have now checked out ${bookRequested} from the ${libObj.name}`
+//     }
+//   }
+// }
+//i want to try removing the first if statement to see if it breaks anything, and just add the second return to the outside of the loop 
+
 function checkoutBook(libObj, bookRequested, genreShelf){
   for  (var i = 0; i < genreShelf.length; i++){
-    if (libObj.shelves[genreShelf].length === 0){
+    if (libObj.shelves[genreShelf][i] === undefined){
       return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`
     } else if (libObj.shelves[genreShelf][i].title === bookRequested){
       libObj.shelves[genreShelf].splice(i, 1);
