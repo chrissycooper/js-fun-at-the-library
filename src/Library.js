6fab4d16c5fa09249964d69dd20d2da409956c22
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-03 16:45:14 -0600

    Refactor checkoutBook function

--- src/Library.js
+++ src/Library.js
@@ -13,30 +13,18 @@ function addBook (nameOfLib, bookToAdd){
   } else if (bookToAdd.genre === 'nonFiction'){
     nameOfLib.shelves.nonFiction.push(bookToAdd);
   } 
-}
-
-// function checkoutBook(libObj, bookRequested, genreShelf){
-//   for  (var i = 0; i < genreShelf.length; i++){
-//     if (libObj.shelves[genreShelf].length === 0){
-//       return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`
-//     } else if (libObj.shelves[genreShelf][i].title === bookRequested){
-//       libObj.shelves[genreShelf].splice(i, 1);
-//       return `You have now checked out ${bookRequested} from the ${libObj.name}`
-//     }
-//   }
-// }
-//i want to try removing the first if statement to see if it breaks anything, and just add the second return to the outside of the loop 
+};
 
 function checkoutBook(libObj, bookRequested, genreShelf){
   for  (var i = 0; i < genreShelf.length; i++){
-    if (libObj.shelves[genreShelf][i] === undefined){
-      return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`
+    if (!libObj.shelves[genreShelf][i]){
+      return `Sorry, there are currently no copies of ${bookRequested} available at the ${libObj.name}`;
     } else if (libObj.shelves[genreShelf][i].title === bookRequested){
       libObj.shelves[genreShelf].splice(i, 1);
-      return `You have now checked out ${bookRequested} from the ${libObj.name}`
+      return `You have now checked out ${bookRequested} from the ${libObj.name}`;
     }
   }
-}
+};
 
 
 
