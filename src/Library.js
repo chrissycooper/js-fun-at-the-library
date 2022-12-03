fba05b590d3bb9ed9ee12f623bda7a54ddb7b996
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-03 10:51:24 -0600

    Pass second addBook test

--- src/Library.js
+++ src/Library.js
@@ -1,12 +1,20 @@
 function createLibrary(nameOfLib){
   return {
     name: nameOfLib,
-    shelves: {fantasy: [], fiction: [], nonFiction: []}
+    shelves: {fantasy: [], fiction: [], nonFiction: [], miscellaneous: []}
   }
 };
 
 function addBook (nameOfLib, bookToAdd){
+  if(bookToAdd.genre === 'fantasy'){
   return nameOfLib.shelves.fantasy.push(bookToAdd);
+  } else if (bookToAdd.genre === 'nonFiction'){
+    return nameOfLib.shelves.nonFiction.push(bookToAdd);
+  } else if (bookToAdd.genre === 'fiction'){
+    return nameOfLib.shelves.fiction.push(bookToAdd);
+  } else {
+    return nameOfLib.shelves.miscellaneous.push(bookToAdd);
+  }
 }
 
 
