fc00e303d03ea332ee2b49a8b62aaaceb43a04ab
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-04 11:31:26 -0600

    Pass first seven tests librarian.js

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,20 @@
+const { addBook } = require("./library");
+
 class Librarian {
-  constructor() {
-    
+  constructor(nameInput, library) {
+    this.name = nameInput;
+    this.library = library;
+  }
+  greetPatron(patron, isMorning){
+    if (isMorning === true){
+      return `Good morning, ${patron}!`;
+    }
+    return `Hello, ${patron}!`;
+  }
+  findBook(bookRequested){
+    if(book.title === bookRequested){
+      return `Yes, we have ${bookRequested}`;
+    }
   }
 }
 
